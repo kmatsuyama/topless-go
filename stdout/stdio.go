de387219ef82fd3ec6299b664f378247ceca08b4
suitai <suitai@outlook.jp>
2018-05-19 19:07:50 +0900

    ファイルを分割

---
+++ stdout/stdio.go
@@ -0,0 +1,240 @@
+package stdout
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	Up      = 'A'
+	Down    = 'B'
+	Right   = 'C'
+	Left    = 'D'
+	Below   = 'E'
+	Above   = 'F'
+	Begin   = 'G'
+	Move    = 'H'
+	Clear   = 'J'
+	Delete  = 'K'
+	Forward = 'S'
+	Back    = 'T'
+)
+
+const (
+	Normal  = "\x1B[0m"
+	Red     = "\x1B[31m"
+	Green   = "\x1B[32m"
+	Yellow  = "\x1B[33m"
+	Blue    = "\x1B[34m"
+	Magenta = "\x1B[35m"
+	Cyan    = "\x1B[36m"
+	White   = "\x1B[37m"
+	BgRed     = "\x1B[41m"
+	BgGreen   = "\x1B[42m"
+	BgYellow  = "\x1B[43m"
+	BgBlue    = "\x1B[44m"
+	BgMagenta = "\x1B[45m"
+	BgCyan    = "\x1B[46m"
+	BgWhite   = "\x1B[47m"
+	RedB     = "\x1B[31;1m"
+	GreenB   = "\x1B[32;1m"
+	YellowB  = "\x1B[33;1m"
+	BlueB    = "\x1B[34;1m"
+	MagentaB = "\x1B[35;1m"
+	CyanB    = "\x1B[36;1m"
+	WhiteB   = "\x1B[37;1m"
+	Red_     = "\x1B[31;4m"
+	Green_   = "\x1B[32;4m"
+	Yellow_  = "\x1B[33;4m"
+	Blue_    = "\x1B[34;4m"
+	Magenta_ = "\x1B[35;4m"
+	Cyan_    = "\x1B[36;4m"
+	White_   = "\x1B[37;1m"
+)
+
+const (
+	CSI = "\033["
+)
+
+const (
+	Before = 0
+	After  = 1
+	All    = 2
+)
+
+const (
+	CountMaxDef = 3
+)
+
+type StrArray struct {
+	elem      []string
+	colorElem []string
+	length    int
+	Height    int
+	width     int
+	count     []int
+}
+
+type printFn func(...interface{}) (n int, err error)
+type printLine func(int, StrArray, printFn) ()
+
+func NewStrArray(str string, delim string, height int, width int) StrArray {
+	elem := strings.Split(str, delim)
+	length := len(elem)
+	if length < height {
+		height = length
+	}
+	return StrArray{
+		elem:   elem,
+		colorElem:  make([]string, length),
+		length: length,
+		Height: height,
+		width: width,
+		count: make([]int, length),
+	}
+}
+
+func csiCode(ctrl rune, num ...int) string {
+	switch len(num) {
+	case 1:
+		return fmt.Sprintf("%s%d%c", CSI, num[0], ctrl)
+	case 2:
+		return fmt.Sprintf("%s%d;%d%c", CSI, num[0], num[1], ctrl)
+	}
+	return ""
+}
+
+func EraseUp(length int) {
+	if length == 0 {
+		return
+	} else if length == 1 {
+		fmt.Print(csiCode(Delete, All))
+		fmt.Print(csiCode(Begin, 1))
+	} else {
+		for i := 0; i < length-1; i++ {
+			fmt.Print(csiCode(Delete, All))
+			fmt.Print(csiCode(Above, 1))
+		}
+	}
+}
+
+func MoveUp(length int) {
+	if length == 0 {
+		return
+	} else if length == 1 {
+		fmt.Print(csiCode(Begin, 1))
+	} else {
+		fmt.Print(csiCode(Above, length-1))
+	}
+}
+
+func wrapIn(width int, line string) string {
+	if len(line) > width {
+		return line[:width]
+	} else {
+		return line
+	}
+}
+
+func New(i int, line StrArray, printF printFn) {
+	fmt.Print(csiCode(Delete, All))
+	printF(wrapIn(line.width, line.elem[i]))
+}
+
+func AsIs(i int, line StrArray, printF printFn) {
+	if line.count[i] > 1 {
+		fmt.Print(csiCode(Delete, All))
+		printF(line.colorElem[i])
+	} else {
+		fmt.Print(csiCode(Delete, All))
+		printF(wrapIn(line.width, line.elem[i]))
+	}
+}
+
+func Changes(i int, line StrArray, printF printFn) {
+	if line.count[i] == CountMaxDef + 1 {
+		fmt.Print(csiCode(Delete, All))
+		printF(line.colorElem[i])
+	} else if line.count[i] == 1 {
+		fmt.Print(csiCode(Delete, All))
+		printF(wrapIn(line.width, line.elem[i]))
+	} else {
+		fmt.Print(csiCode(Below, 1))
+	}
+}
+
+func Lines(line StrArray, head int, printL printLine) {
+	last := line.Height + head - 1
+	for i := head; i < last; i++ {
+		printL(i, line, fmt.Println)
+	}
+	printL(last, line, fmt.Print)
+}
+
+func min(a, b int) int {
+   if a > b {
+      return b
+   }
+   return a
+}
+
+func coloring(color string, line string) string {
+	return color + line + Normal
+}
+
+func colorDiff(orgColor string, color string, oldLine string, line string) string {
+	var same bool
+
+	colorStr := coloring(orgColor, line)
+	num := len(orgColor)
+
+	for i := 0; i < min(len(oldLine), len(line)); i++ {
+		if line[i] == oldLine[i] && !same {
+			colorStr = colorStr[:num] + Normal + orgColor + colorStr[num:]
+			num += len(Normal + orgColor)
+			same = true
+		} else if line[i] != oldLine[i] && same {
+			colorStr = colorStr[:num] + color + colorStr[num:]
+			num += len(color)
+			same = false
+		}
+		num++
+	}
+	return colorStr
+}
+
+func CheckHead(line StrArray, head int, dhead int, height int) int {
+	if line.length < height {
+		return 0
+	}
+	head = head + dhead
+	if head < 0 {
+		head = 0
+	} else if height+head > line.length {
+		head = line.length - height
+	}
+	return head
+}
+
+func checkLineCount(line StrArray, i int) int {
+	if line.count[i] > 0 {
+		return line.count[i]-1
+	} else {
+		return line.count[i]
+	}
+}
+
+func CheckChange(oldLine StrArray, line StrArray) StrArray {
+	for i := 0; i < min(oldLine.length, line.length); i++ {
+		if oldLine.elem[i] == line.elem[i] {
+			line.count[i] = checkLineCount(oldLine, i)
+			if line.count[i] > 1 {
+				line.colorElem[i] = oldLine.colorElem[i]
+			}
+		} else {
+			line.colorElem[i] = colorDiff(Red, Red_, wrapIn(oldLine.width, oldLine.elem[i]),  wrapIn(line.width, line.elem[i]))
+			line.count[i] = CountMaxDef + 1
+		}
+	}
+	return line
+}
--- topless.go
+++ topless.go
@@ -11,18 +11,13 @@ import (
 	"strings"
 	"time"
 	"./ioctl"
+	"./stdout"
 )
 
 const (
 	StdinBuf = 128
 	SleepSecDef = 1
 	CountMaxDef = 3
-)
-
-const (
-	Before = 0
-	After  = 1
-	All    = 2
 )
 
 const (
@@ -48,47 +43,6 @@ const (
 	DownKey = "\033[B"
 )
 
-const (
-	Normal  = "\x1B[0m"
-	Red     = "\x1B[31m"
-	Green   = "\x1B[32m"
-	Yellow  = "\x1B[33m"
-	Blue    = "\x1B[34m"
-	Magenta = "\x1B[35m"
-	Cyan    = "\x1B[36m"
-	White   = "\x1B[37m"
-	BgRed     = "\x1B[41m"
-	BgGreen   = "\x1B[42m"
-	BgYellow  = "\x1B[43m"
-	BgBlue    = "\x1B[44m"
-	BgMagenta = "\x1B[45m"
-	BgCyan    = "\x1B[46m"
-	BgWhite   = "\x1B[47m"
-	RedB     = "\x1B[31;1m"
-	GreenB   = "\x1B[32;1m"
-	YellowB  = "\x1B[33;1m"
-	BlueB    = "\x1B[34;1m"
-	MagentaB = "\x1B[35;1m"
-	CyanB    = "\x1B[36;1m"
-	WhiteB   = "\x1B[37;1m"
-	Red_     = "\x1B[31;4m"
-	Green_   = "\x1B[32;4m"
-	Yellow_  = "\x1B[33;4m"
-	Blue_    = "\x1B[34;4m"
-	Magenta_ = "\x1B[35;4m"
-	Cyan_    = "\x1B[36;4m"
-	White_   = "\x1B[37;1m"
-)
-
-type strArray struct {
-	elem      []string
-	colorElem []string
-	length    int
-	height    int
-	width     int
-	count     []int
-}
-
 type optToCmd struct {
 	sleepSec float64
 	force    bool
@@ -102,35 +56,6 @@ type stdinToCmd struct {
 type stdinToPrint struct {
 	refresh chan bool
 	head    chan int
-}
-
-type printFn func(...interface{}) (n int, err error)
-type printLine func(int, strArray, printFn) ()
-
-func newStrArray(str string, delim string, height int, width int) strArray {
-	elem := strings.Split(str, delim)
-	length := len(elem)
-	if length < height {
-		height = length
-	}
-	return strArray{
-		elem:   elem,
-		colorElem:  make([]string, length),
-		length: length,
-		height: height,
-		width: width,
-		count: make([]int, length),
-	}
-}
-
-func csiCode(ctrl rune, num ...int) string {
-	switch len(num) {
-	case 1:
-		return fmt.Sprintf("%s%d%c", CSI, num[0], ctrl)
-	case 2:
-		return fmt.Sprintf("%s%d;%d%c", CSI, num[0], num[1], ctrl)
-	}
-	return ""
 }
 
 func getStdin(stdinChan chan<- string) {
@@ -262,144 +187,9 @@ func runCmdRepeatedly(cmdstr []string, cmdoutChan chan<- string, chanCmd stdinTo
 	return err
 }
 
-func min(a, b int) int {
-   if a > b {
-      return b
-   }
-   return a
-}
-
-func checkHead(line strArray, head int, dhead int, height int) int {
-	if line.length < height {
-		return 0
-	}
-	head = head + dhead
-	if head < 0 {
-		head = 0
-	} else if height+head > line.length {
-		head = line.length - height
-	}
-	return head
-}
-
-func eraseToBegin(length int) {
-	if length == 0 {
-		return
-	} else if length == 1 {
-		fmt.Print(csiCode(Delete, All))
-		fmt.Print(csiCode(Begin, 1))
-	} else {
-		for i := 0; i < length-1; i++ {
-			fmt.Print(csiCode(Delete, All))
-			fmt.Print(csiCode(Above, 1))
-		}
-	}
-}
-
-func moveToBegin(length int) {
-	if length == 0 {
-		return
-	} else if length == 1 {
-		fmt.Print(csiCode(Begin, 1))
-	} else {
-		fmt.Print(csiCode(Above, length-1))
-	}
-}
-
-func wrapIn(width int, line string) string {
-	if len(line) > width {
-		return line[:width]
-	} else {
-		return line
-	}
-}
-
-func coloring(color string, line string) string {
-	return color + line + Normal
-}
-
-func colorDiff(orgColor string, color string, oldLine string, line string) string {
-	var same bool
-
-	colorStr := coloring(orgColor, line)
-	num := len(orgColor)
-
-	for i := 0; i < min(len(oldLine), len(line)); i++ {
-		if line[i] == oldLine[i] && !same {
-			colorStr = colorStr[:num] + Normal + orgColor + colorStr[num:]
-			num += len(Normal + orgColor)
-			same = true
-		} else if line[i] != oldLine[i] && same {
-			colorStr = colorStr[:num] + color + colorStr[num:]
-			num += len(color)
-			same = false
-		}
-		num++
-	}
-	return colorStr
-}
-
-func printNew(i int, line strArray, print printFn) {
-	fmt.Print(csiCode(Delete, All))
-	print(wrapIn(line.width, line.elem[i]))
-}
-
-func printAsIs(i int, line strArray, print printFn) {
-	if line.count[i] > 1 {
-		fmt.Print(csiCode(Delete, All))
-		print(line.colorElem[i])
-	} else {
-		fmt.Print(csiCode(Delete, All))
-		print(wrapIn(line.width, line.elem[i]))
-	}
-}
-
-func printChanges(i int, line strArray, print printFn) {
-	if line.count[i] == CountMaxDef + 1 {
-		fmt.Print(csiCode(Delete, All))
-		print(line.colorElem[i])
-	} else if line.count[i] == 1 {
-		fmt.Print(csiCode(Delete, All))
-		print(wrapIn(line.width, line.elem[i]))
-	} else {
-		fmt.Print(csiCode(Below, 1))
-	}
-}
-
-func printLines(line strArray, head int, print printLine) {
-	last := line.height + head - 1
-	for i := head; i < last; i++ {
-		print(i, line, fmt.Println)
-	}
-	print(last, line, fmt.Print)
-}
-
-func checkLineCount(line strArray, i int) int {
-	if line.count[i] > 0 {
-		return line.count[i]-1
-	} else {
-		return line.count[i]
-	}
-}
-
-func checkChangeLine(oldLine strArray, line strArray) strArray {
-	for i := 0; i < min(oldLine.length, line.length); i++ {
-		if oldLine.elem[i] == line.elem[i] {
-			line.count[i] = checkLineCount(oldLine, i)
-			if line.count[i] > 1 {
-				line.colorElem[i] = oldLine.colorElem[i]
-			}
-		} else {
-			line.colorElem[i] = colorDiff(Red, Red_, wrapIn(oldLine.width, oldLine.elem[i]),  wrapIn(line.width, line.elem[i]))
-			line.count[i] = CountMaxDef + 1
-		}
-	}
-	return line
-}
-
 func printRepeatedly(cmdoutChan <-chan string, chanPrint stdinToPrint) {
-	var oldLine strArray
-	var line strArray
+	var oldLine stdout.StrArray
+	var line stdout.StrArray
 	var cmdout string
 	var height, width int
 	var head int
@@ -414,26 +204,26 @@ func printRepeatedly(cmdoutChan <-chan string, chanPrint stdinToPrint) {
 		select {
 		case refresh := <-chanPrint.refresh:
 			if refresh {
-				eraseToBegin(oldLine.height)
-				printLines(oldLine, head, printNew)
+				stdout.EraseUp(oldLine.Height)
+				stdout.Lines(oldLine, head, stdout.New)
 			}
 		case dHead := <-chanPrint.head:
-			newHead := checkHead(oldLine, head, dHead, height)
+			newHead := stdout.CheckHead(oldLine, head, dHead, height)
 			if newHead != head {
 				head = newHead
-				eraseToBegin(oldLine.height)
-				printLines(oldLine, head, printAsIs)
+				stdout.EraseUp(oldLine.Height)
+				stdout.Lines(oldLine, head, stdout.AsIs)
 			}
 		case cmdout = <-cmdoutChan:
-			line = newStrArray(cmdout, "\n", height, width)
-			head = checkHead(line, head, 0, height)
-			if oldLine.height != line.height {
-				eraseToBegin(oldLine.height)
-				printLines(line, head, printNew)
+			line = stdout.NewStrArray(cmdout, "\n", height, width)
+			head = stdout.CheckHead(line, head, 0, height)
+			if oldLine.Height != line.Height {
+				stdout.EraseUp(oldLine.Height)
+				stdout.Lines(line, head, stdout.New)
 			} else {
-				line = checkChangeLine(oldLine, line)
-				moveToBegin(oldLine.height)
-				printLines(line, head, printChanges)
+				line = stdout.CheckChange(oldLine, line)
+				stdout.MoveUp(oldLine.Height)
+				stdout.Lines(line, head, stdout.Changes)
 			}
 			oldLine = line
 		}
